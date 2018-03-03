7429ab94323a6374d6f1db7678ea2931f51fead8
jcremona <jcremona@labdcc.fceia.unr.edu.ar>
2018-03-03 20:14:35 -0300

    Fix bug commit previo

--- vgg_trainable/test/test_model.py
+++ vgg_trainable/test/test_model.py
@@ -13,36 +13,36 @@ from main import fill_feed_dict
 DEFAULT_INTRINSIC_FILE_NAME = "intrinsic_matrix.txt"
 
 def test_model(model_name, intrinsic_matrix, data_dir):
-    sess = tf.Session()
-    saver = tf.train.import_meta_graph(model_name+".meta")
-    #print(model_name)
-    inverse_intrinsic_matrix = np.linalg.inv(intrinsic_matrix)
-    saver.restore(sess, model_name)#tf.train.latest_checkpoint('./'))  
-    graph = tf.get_default_graph()
-    outputs = graph.get_tensor_by_name("outputs:0")
-    targets_placeholder = graph.get_tensor_by_name("targets_placeholder:0")
-    images_placeholder = graph.get_tensor_by_name("images_placeholder:0") 
-    batch_size = 100
-    images, targets, _ = read_data_sets(data_dir)
-    dataset = DataSet(images, targets, fake_data=False)
-    steps_per_epoch = dataset.num_examples // batch_size
-    num_examples = steps_per_epoch * batch_size
-    relative_poses_prediction = np.empty((num_examples, 12))
-    relative_poses_target = np.empty((num_examples, 12))
-    for step in xrange(steps_per_epoch):
+	sess = tf.Session()
+	saver = tf.train.import_meta_graph(model_name+".meta")
+	#print(model_name)
+	inverse_intrinsic_matrix = np.linalg.inv(intrinsic_matrix)
+	saver.restore(sess, model_name)#tf.train.latest_checkpoint('./'))  
+	graph = tf.get_default_graph()
+	outputs = graph.get_tensor_by_name("outputs:0")
+	targets_placeholder = graph.get_tensor_by_name("targets_placeholder:0")
+	images_placeholder = graph.get_tensor_by_name("images_placeholder:0") 
+	batch_size = 100
+	images, targets, _ = read_data_sets(data_dir)
+	dataset = DataSet(images, targets, fake_data=False)
+	steps_per_epoch = dataset.num_examples // batch_size
+	num_examples = steps_per_epoch * batch_size
+	relative_poses_prediction = np.empty((num_examples, 12))
+	relative_poses_target = np.empty((num_examples, 12))
+	for step in xrange(steps_per_epoch):
 		feed_dict = fill_feed_dict(dataset, images_placeholder, targets_placeholder, feed_with_batch=True, batch_size=batch_size, shuffle=False, standardize_targets=True)
-		prediction_batch, target_batch, images = sess.run([outputs, targets_placeholder, images_placeholder], feed_dict=feed_dict)
+		prediction_batch, target_batch = sess.run([outputs, targets_placeholder], feed_dict=feed_dict)
 		batch_relative_poses_pred, batch_relative_poses_target = get_trajectories(dataset, batch_size, inverse_intrinsic_matrix, prediction_batch, target_batch)
 		init = batch_size * step
 		end = batch_size * (step + 1)
 		relative_poses_prediction[init:end] = batch_relative_poses_pred
 		relative_poses_target[init:end] = batch_relative_poses_target
-    np.savetxt("relative_poses_prediction.txt", relative_poses_prediction, delimiter=' ')
-    np.savetxt("relative_poses_target.txt", relative_poses_target, delimiter=' ')
+	np.savetxt("relative_poses_prediction.txt", relative_poses_prediction, delimiter=' ')
+	np.savetxt("relative_poses_target.txt", relative_poses_target, delimiter=' ')
 	absolute_poses_prediction = get_absolute_poses(relative_poses_prediction.reshape((num_examples,3,4)))
 	absolute_poses_target = get_absolute_poses(relative_poses_target.reshape((num_examples,3,4)))
-    np.savetxt("absolute_poses_prediction.txt", absolute_poses_prediction, delimiter=' ')
-    np.savetxt("absolute_poses_target.txt", absolute_poses_target, delimiter=' ')
+	np.savetxt("absolute_poses_prediction.txt", absolute_poses_prediction.reshape(num_examples,12), delimiter=' ')
+	np.savetxt("absolute_poses_target.txt", absolute_poses_target.reshape(num_examples,12), delimiter=' ')
 
 
 def get_trajectories(dataset, batch_size, inverse_intrinsic_matrix, prediction_batch, target_batch):
@@ -59,7 +59,7 @@ def get_trajectories(dataset, batch_size, inverse_intrinsic_matrix, prediction_b
 		u,_ = linalg.polar(pred_transformation[0:3,0:3])
 		pred_transf_correction = np.empty((3,4))
 		pred_transf_correction[0:3, 0:3] = u
-		pred_transf_correction[0:3, 3] = pred_transformation[0:3,3]
+		pred_transf_correction[0:3, 3] = pred_transformation[0:3,3].transpose()
 
 		target = target_batch[i]
 		target = target * dataset.targets_std + dataset.targets_mean
@@ -77,8 +77,10 @@ def get_absolute_poses(relative_poses):
 	num_examples = relative_poses.shape[0]
 	absolute_poses = np.empty(shape=relative_poses.shape)
 	for i in xrange(num_examples):
-		current = current * relative_poses[i]
-		absolute_poses[i] = current
+		T = np.matrix(np.identity(4))
+		T[0:3,:] = relative_poses[i]
+		current = current * np.linalg.inv(T)
+		absolute_poses[i] = current[0:3,:]
 	return absolute_poses
 
 def show_images(images, cols = 1, titles = None):
