eb8170abec4931f3315050e55fdf7cb6a75cddd3
jcremona <jcremona>
2018-03-13 14:57:59 -0300

    Revert "Ref #17: implementa un learning rate adaptativo, disminuye hasta 4 ordenes de magnitud"
    This reverts commit 5398c707dddb84005f022ef118c178adb3e09dc8.

--- vgg_trainable/model.py
+++ vgg_trainable/model.py
@@ -157,25 +157,13 @@ def training(loss, learning_rate):
   # Add a scalar summary for the snapshot loss.
   tf.summary.scalar('loss', loss)
 
-  #optimizer = tf.train.AdamOptimizer(learning_rate)
-
+  optimizer = tf.train.AdamOptimizer(learning_rate)
   # Create a variable to track the global step.
   global_step = tf.Variable(0, name='global_step', trainable=False)
-  
-  decay_learning_rate = get_learning_rate(learning_rate, global_step)
-  tf.summary.scalar('learning_rate', decay_learning_rate)
-  optimizer = tf.train.AdamOptimizer(decay_learning_rate)
-  # Use the optimizer to minimize the loss
+  # Use the optimizer to apply the gradients that minimize the loss
   # (and also increment the global step counter) as a single training step.
   train_op = optimizer.minimize(loss, global_step=global_step)
   return train_op
-
-
-def get_learning_rate(initial_learning_rate, global_step):
-    boundaries = [6000, 20000, 35000]
-    values = [initial_learning_rate, initial_learning_rate / 10., initial_learning_rate / 100., initial_learning_rate / 1000.]
-    return tf.train.piecewise_constant(global_step, boundaries, values)
-
 
 # Deprecated
 def evaluation(outputs, targets):
